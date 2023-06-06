cc08e52a53203297cfa909393018ddd72dafb5bf
“Oscar” <os0c40raa@gmail.com>
2023-06-05 20:35:04 -0600

    Repositorio base inicial

---
+++ topico/src/main/java/com/one/alura/topico/TopicoApplication.java
@@ -0,0 +1,13 @@
+package com.one.alura.topico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicoApplication.class, args);
+	}
+
+}
---
+++ topico/src/test/java/com/one/alura/topico/TopicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.alura.topico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
